c92b8a55a57b0964e964fbbb0f20b0420803a47b
Mark <markjnpr685@gmail.com>
2014-03-02 15:55:28 +0000

    Initialise first rails repogit status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'e5ed970937e636dc84560ab1b561085474c13c68d343b3dbb44603827dcf0dddd8f1ebc6adfbcf23a8424d46b2a0100fb326922d8fd8216ea4881501090ac53c'
